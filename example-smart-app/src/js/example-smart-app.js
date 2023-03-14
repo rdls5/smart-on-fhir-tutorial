2a964ced3020cbfa81163c7f9ea18e80ac161bfe
rdls5 <127534772+rdls5@users.noreply.github.com>
2023-03-14 13:54:29 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -27,7 +27,7 @@
                   });
         
         console.log('smart own property: ' + smart.hasOwnProperty('patient'));
-        console.log('observation value: ' + JSON.stringify(code));
+      
         console.log('obv or value: ' + JSON.stringify($or));
 
         
