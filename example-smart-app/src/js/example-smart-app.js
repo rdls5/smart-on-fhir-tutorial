3b17a045af4f64d0291586e64c9e01d39289f629
rdls5 <127534772+rdls5@users.noreply.github.com>
2023-03-22 09:25:55 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -174,7 +174,7 @@
   };
   
   window.drawVisualizationPatient = function(pr) {
-    console.log('PATIENT RES FUNCTION: ' + '\n' + pr);
+    console.log('PATIENT RESource FUNCTION: ' + '\n' + pr);
     $('#holder').show();
     $('#loading').hide();
     $('#pr.ptres').html(pr);
